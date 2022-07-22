1b90502fbff3a42a7bec8bce9f688fc234b6f893
Madhura Mihiranga <it19177410@my.sliit.lk>
2022-07-22 11:29:42 +0530

    remove

--- StudentServer/src/main/java/com/studentserver/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.studentserver.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.studentserver.model.UserDao;
-public interface UserRepository extends CrudRepository<UserDao, Integer> {
-    UserDao findByUsername(String username);
-}
